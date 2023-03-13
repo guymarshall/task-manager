1d44355344c854df11fdf9bd07c70a277041b2e2
Guy Marshall <guymarshall9@gmail.com>
2023-03-13 22:34:42 +0000

    Change VARCHAR(255) to TEXT

--- db_init.sql
+++ db_init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     userID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    emailAddress VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    emailAddress TEXT NOT NULL,
+    password TEXT NOT NULL,
     createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     isDeleted INT NOT NULL DEFAULT 0
 );
@@ -14,8 +14,8 @@ VALUES (
 
 CREATE TABLE tasks (
     taskID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
+    title TEXT NOT NULL,
+    description TEXT,
     assignedToID INT,
     createdByID INT NOT NULL,
     createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
