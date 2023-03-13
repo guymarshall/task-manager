0755d525dafdbcd6c338bb9b6ec3e7ccaf1b8953
Guy Marshall <guymarshall9@gmail.com>
2023-03-13 22:34:15 +0000

    Add tasks table to script db_init.sql

--- db_init.sql
+++ db_init.sql
@@ -11,3 +11,13 @@ VALUES (
     SHA2('user@example.com', 256),
     SHA2('password123', 256)
 );
+
+CREATE TABLE tasks (
+    taskID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    assignedToID INT,
+    createdByID INT NOT NULL,
+    createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    isDeleted BOOLEAN NOT NULL DEFAULT FALSE
+);
