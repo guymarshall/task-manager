08b74769d9f69d2fa50d7a7db9ff693968bc1ffd
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-14 12:19:24 +0000

    Add basic code to show how Database.php might be used in index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
-echo 'Hello, World!';
+require_once 'models/Database.php';
+
+$table_name = "users";
+$data = array("name" => "John", "email" => "john@example.com", "age" => 30);
+
+insert_data($table_name, $data);
