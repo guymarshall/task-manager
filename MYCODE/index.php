9d0b4246e366f65aaeadb206e5abf600c1d4bfc5
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-16 12:31:38 +0000

    Remove MYCODE directory

--- MYCODE/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'models/Database.php';
-
-$table_name = "users";
-$data = array("name" => "John", "email" => "john@example.com", "age" => 30);
-
-insert_data($table_name, $data);
